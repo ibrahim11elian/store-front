127f8b32bfcd552c2361c3ea2ba03e70ceb3801e
Ibrahim Ahmed Abdelnaby <58626527+ibrahim11elian@users.noreply.github.com>
2022-08-20 22:13:50 +0200

    user CRUD added

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -6,10 +6,15 @@ import user from './api/user';
 const route = Router();
 
 // user routes
+// create new user
 route.post('/user', user.create);
+// get all users tonken required
 route.get('/users', jwtAuthenticate, user.index);
+// get user by his user name, tonken required
 route.get('/user/:userName', jwtAuthenticate, user.getUser);
+// update user data by his user name, user tonken required
 route.put('/user/:userName', jwtAuthorize, user.updateUser);
+// delete user by his user name, user tonken required
 route.delete('/user/:userName', jwtAuthorize, user.deleteUser);
 
 export default route;
--- src/utilities/password_hashing.ts
+++ src/utilities/password_hashing.ts
@@ -5,6 +5,8 @@ dotenv.config();
 
 const { SALT_ROUNDS } = process.env;
 
+// hash user password
+
 export function hash(pass: string): string {
   try {
     const password_digest = bcrypt.hashSync(pass, Number(SALT_ROUNDS));
